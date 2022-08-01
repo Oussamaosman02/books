9ac5abe4659e62c4e5ed3ed9e3f026119dfd631d
oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2022-08-01 12:30:58 +0200

    adding books, editing info of some of them and adding images

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import "./App.css";
-import data from "./datos.json";
+import Lista from "./componentes/lista";
+import logo from "./libro.png";
 
 function App() {
-  let dat = data;
+  
   return (
     <div className="App">
+      <div className="imag">
+        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" />
+      </div>
       <h1>Libros</h1>
-      <ul>
-        {dat.map((d) => {
-          return (
-            <li key={`${d.palabras} ${d.paginas}`} className={d.leido}>
-              <span>
-                <h2>{d.titulo}</h2>
-                <h3>{d.autor}</h3>
-                <h6>{d.fecha}</h6>
-              </span>
-              <span>
-                <h4>Paginas: {d.paginas}</h4>
-                <h5>Palabras: {d.palabras}</h5>
-                <h6 className="fecha">
-                  Tiempo:{" "}
-                  {d.tiempo ? d.tiempo : Math.round(d.palabras / 140 / 60)}
-                </h6>
-              </span>
-            </li>
-          );
-        })}
-      </ul>
+      <Lista/>
     </div>
   );
 }
