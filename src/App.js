480dacace1fe8a76900900c5c5fc49b58b1cb9ec
oussama02 <oussamaosman02@gmail.com>
2022-11-12 14:19:25 +0100

    Updating to use standard lint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import "./App.css";
-import Lista from "./componentes/lista";
-import logo from "./libro.webp";
+import './App.css'
+import Lista from './componentes/lista'
+import logo from './libro.webp'
 
-function App() {
-  
+function App () {
   return (
-    <div className="App">
-      <div className="imag">
-        <img src={logo} alt="Logo" />
-        <img src={logo} alt="Logo" />
+    <div className='App'>
+      <div className='imag'>
+        <img src={logo} alt='Logo' />
+        <img src={logo} alt='Logo' />
       </div>
       <h1>Libros</h1>
-      <Lista/>
+      <Lista />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/componentes/lista.js
+++ src/componentes/lista.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./lista.css";
-import data from "../datos.json";
+import React from 'react'
+import './lista.css'
+import data from '../datos.json'
 
-export default function Lista() {
-  let dat = data;
+export default function Lista () {
+  const dat = data
   return (
     <ul>
       {dat
         .sort(function () {
-          return Math.random() - 0.5;
+          return Math.random() - 0.5
         })
         .map((d) => {
           return (
@@ -22,14 +22,14 @@ export default function Lista() {
               <span>
                 <h4>Paginas: {d.paginas}</h4>
                 <h5>Palabras: {d.palabras}</h5>
-                <h6 className="tiempo">
-                  Tiempo:{" "}
+                <h6 className='tiempo'>
+                  Tiempo:{' '}
                   {d.tiempo ? d.tiempo : Math.round(d.palabras / 140 / 60)}
                 </h6>
               </span>
             </li>
-          );
+          )
         })}
     </ul>
-  );
+  )
 }
